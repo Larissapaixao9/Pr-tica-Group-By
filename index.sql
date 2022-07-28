b78cfdce42d4f1fe6f4395a7c65d587778fe1ccf
Larissa Paixao <laryssapaixao9@gmail.com>
2022-07-28 15:56:55 -0300

    Finaliza questões sem bônus

--- index.sql
+++ index.sql
@@ -1,1 +1,12 @@
 --Questao1:
+SELECT COUNT(experiences."endDate") AS "currentExperiences"  FROM experiences ;
+
+--Questão 2:
+SELECT COUNT(id) AS "Educations", educations."userId"  FROM educations Group by "userId";
+
+--Questão 3:
+SELECT COUNT(testimonials.message) AS "testimonailCount", users.name FROM testimonials JOIN users 
+ON testimonials."writerId"=users.id WHERE testimonials."writerId"=435 GROUP BY testimonials."writerId", users.name;
+
+--Questão 4:
+SELECT MAX(salary) AS "maximumSalary",roles.name AS cargo FROM jobs JOIN roles ON jobs."roleId"=roles.id WHERE jobs.active=true  GROUP BY cargo ORDER BY "maximumSalary" ASC;
